b445db28bee7a12949b5df4f89419f5dc15ef922
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-12 15:03:03 +0100

    netG passed the unittest

--- unittest/test.py
+++ unittest/test.py
@@ -1,19 +1,40 @@
 import os
 import sys
+import torch
+import torch.nn as nn
 import unittest
 
 
 sys.path.append("./src")
 
-from src.generator import CoupledGenerators
+from utils import config
+from generator import CoupledGenerators
+
 
 class UnitTest(unittest.TestCase):
     def setUp(self):
-        return super().setUp()
-    
+        self.image_size = config()["dataloader"]["image_size"]
+        self.batch_size = config()["dataloader"]["batch_size"]
+
+        self.latent_space = config()["netG"]["latent_space"]
+        self.constant = config()["netG"]["constant"]
+
+        self.netG = CoupledGenerators(
+            latent_space=self.latent_space,
+            constant=self.constant,
+            image_size=self.image_size,
+        )
+
     def test_coupleGenerator(self):
-        pass
-    
-    
-if __name__ == '__main__':
+        self.Z = torch.randn(self.batch_size, self.latent_space)
+
+        image1, image2 = self.netG(self.Z)
+
+        self.assertEqual(
+            image1.size(), image2.size(), "Image1 and Image2 must be the same size"
+        )
+
+
+if __name__ == "__main__":
+
     unittest.main()
