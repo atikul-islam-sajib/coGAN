ac634e9c2b90799c4121233228cfc6772b349216
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-12 16:04:57 +0100

    netD passed the unittest

--- unittest/test.py
+++ unittest/test.py
@@ -9,6 +9,7 @@ sys.path.append("./src")
 
 from utils import config
 from generator import CoupledGenerators
+from discriminator import CoupledDiscriminators
 
 
 class UnitTest(unittest.TestCase):
@@ -25,6 +26,12 @@ class UnitTest(unittest.TestCase):
             image_size=self.image_size,
         )
 
+        self.netD = CoupledDiscriminators(
+            channels=3,
+            constant=self.constant,
+            image_size=self.image_size,
+        )
+
     def test_coupleGenerator(self):
         self.Z = torch.randn(self.batch_size, self.latent_space)
 
@@ -32,6 +39,19 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(
             image1.size(), image2.size(), "Image1 and Image2 must be the same size"
+        )
+
+    def test_coupledDiscriminator(self):
+        self.Z = torch.randn(self.batch_size, self.latent_space)
+
+        image1, image2 = self.netG(self.Z)
+
+        validity1, validity2 = self.netD(image1=image1, image2=image2)
+
+        self.assertEqual(
+            validity1.size(),
+            validity2.size(),
+            "Validity1 and Validity2 must be the same size",
         )
 
 
