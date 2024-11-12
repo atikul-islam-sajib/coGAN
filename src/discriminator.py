b8e1bbaa361d6c904d036571e45ef36646daac69
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-12 15:44:15 +0100

    netD is updated

--- src/discriminator.py
+++ src/discriminator.py
@@ -50,11 +50,26 @@ class CoupledDiscriminators(nn.Module):
 
         self.sharedConvolution = nn.Sequential(*self.layers)
 
+        self.discriminator1 = nn.Linear(
+            in_features=self.constant * (self.image_size // 2**4) ** 2,
+            out_features=self.in_channels // self.in_channels,
+        )
+        
+        self.discriminator2 = nn.Linear(
+            in_features=self.constant * (self.image_size // 2**4) ** 2,
+            out_features=self.in_channels // self.in_channels,
+        )
+
     def forward(self, image1: torch.Tensor, image2: torch.Tensor):
         if isinstance(image1, torch.Tensor) and isinstance(image2, torch.Tensor):
             shared = self.sharedConvolution(image1)
+
+            shared = shared.view(shared.size(0), -1)
             
-            return shared
+            validity1 = self.discriminator1(shared)
+            validity2 = self.discriminator2(shared)
+            
+            return validity1, validity2
 
         else:
             raise ValueError("Both inputs must be PyTorch tensors".capitalize())
@@ -62,4 +77,10 @@ class CoupledDiscriminators(nn.Module):
 
 if __name__ == "__main__":
     netD = CoupledDiscriminators()
-    print(netD(image1 = torch.randn((64, 3, 32, 32)), image2 = torch.randn((64, 3, 32, 32))).size())
+    validity1, validity2 = netD(
+            image1=torch.randn((64, 3, 32, 32)), image2=torch.randn((64, 3, 32, 32))
+        )
+    
+    
+    print("Validity1 size # {}".format(validity1.size()))
+    print("Validity2 size # {}".format(validity2.size()))
