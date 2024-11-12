8e86167d37e7b0721012e0d99a6a0729752f54fd
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-12 15:36:29 +0100

    shared discriminator is added

--- src/discriminator.py
+++ src/discriminator.py
@@ -19,16 +19,47 @@ class CoupledDiscriminators(nn.Module):
         super(CoupledDiscriminators, self).__init__()
 
         self.in_channels = channels
+        self.out_channels = self.in_channels * 5 + 1
         self.image_size = image_size
         self.constant = constant
 
+        self.kernel_size = 3
+        self.stride_size = 2
+        self.padding_size = 1
+
+        self.layers = []
+
+        for index in range(4):
+            self.layers += [
+                nn.Conv2d(
+                    in_channels=self.in_channels,
+                    out_channels=self.out_channels,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride_size,
+                    padding=self.padding_size,
+                ),
+            ]
+
+            if index != 0:
+                self.layers += [nn.BatchNorm2d(num_features=self.out_channels)]
+
+            self.layers += [nn.LeakyReLU(negative_slope=0.2, inplace=True)]
+
+            self.in_channels = self.out_channels
+            self.out_channels = self.in_channels * 2
+
+        self.sharedConvolution = nn.Sequential(*self.layers)
+
     def forward(self, image1: torch.Tensor, image2: torch.Tensor):
         if isinstance(image1, torch.Tensor) and isinstance(image2, torch.Tensor):
-            pass
+            shared = self.sharedConvolution(image1)
+            
+            return shared
 
         else:
             raise ValueError("Both inputs must be PyTorch tensors".capitalize())
 
 
 if __name__ == "__main__":
-    pass
+    netD = CoupledDiscriminators()
+    print(netD(image1 = torch.randn((64, 3, 32, 32)), image2 = torch.randn((64, 3, 32, 32))).size())
