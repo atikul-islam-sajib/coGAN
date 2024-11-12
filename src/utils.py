67a1af81fea778a615c6c2345307747775a1ba58
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-12 16:34:46 +0100

    device init is completed

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import torch
 import torch.nn as nn
 
 
@@ -17,3 +18,12 @@ def weight_init(m):
     elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
+
+
+def device_init(device: str = "cuda"):
+    if device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    elif device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
