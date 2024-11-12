e06b1df83b8645101ee03efcda36a731b12ab8e2
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-12 16:32:59 +0100

    weight init is completed in the utils file

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,19 @@
 import os
 import sys
 import yaml
+import torch.nn as nn
 
 
 def config():
     with open("./config.yml", "r") as file:
         return yaml.safe_load(file)
+
+
+def weight_init(m):
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
