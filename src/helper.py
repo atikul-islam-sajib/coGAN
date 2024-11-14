5012618af236c0844b7209af7d94efee6d05afda
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-11-14 13:21:39 +0100

    helper code is updated

--- src/helper.py
+++ src/helper.py
@@ -3,17 +3,31 @@ import sys
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.utils
 
 sys.path.append("./src/")
 
-from utils import config
+from utils import config, load
 from loss import AdversarialLoss
 from generator import CoupledGenerators
 from discriminator import CoupledDiscriminators
 
 
 def load_dataset():
-    pass
+    train_dataloader = os.path.join(
+        config()["path"]["processed_path"], "train_dataloader.pkl"
+    )
+    valid_dataloader = os.path.join(
+        config()["path"]["processed_path"], "valid_dataloader.pkl"
+    )
+
+    train_dataloader = load(filename=train_dataloader)
+    valid_dataloader = load(filename=valid_dataloader)
+
+    return {
+        "train_dataloader": train_dataloader,
+        "valid_dataloader": valid_dataloader,
+    }
 
 
 def helper(**kwargs):
@@ -53,7 +67,11 @@ def helper(**kwargs):
         name="Adversarial Loss for coupledGAN".title(), reduction=reduction
     )
 
+    dataloader = load_dataset()
+
     return {
+        "train_dataloader": dataloader["train_dataloader"],
+        "valid_dataloader": dataloader["valid_dataloader"],
         "netG": netG,
         "netD": netD,
         "optimizerG": optimizerG,
@@ -73,19 +91,21 @@ if __name__ == "__main__":
         reduction="mean",
     )
 
+    assert init["train_dataloader"].__class__ == torch.utils.data.DataLoader
+    assert init["valid_dataloader"].__class__ == torch.utils.data.DataLoader
     assert (
         init["netG"].__class__ == CoupledGenerators
     ), "netG should be coupledGenerators".title()
     assert (
         init["netD"].__class__ == CoupledDiscriminators
     ), "netD should be coupledDiscriminators".title()
-    
+
     # assert init["optimizerG"].__class__ == optim.Adam, "optimizerG should be Adam".title()
     # assert init["optimizerD"].__class__ == optim.Adam, "optimizerD should be Adam".title()
-    
+
     assert init["optimizerG"].__class__ == optim.SGD, "optimizerG should be SGD".title()
     assert init["optimizerD"].__class__ == optim.SGD, "optimizerD should be SGD".title()
-    
+
     assert (
         init["adversarial_loss"].__class__ == AdversarialLoss
     ), "adversarial_loss should be AdversarialLoss".title()
